09e4900fc58eb88d3754757e0b7f5abefd545023
zooksman <zooksman@gmail.com>
2022-12-31 18:03:59 -0600

    trying to get adblock script to work

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -2,15 +2,15 @@
 const head = document.getElementsByTagName('head')[0];
 
 // Inject BTTV if enabled
-// Disabled by zooksman on 31.12.22
-//browser.runtime.sendMessage({ type: "GetStoreExecution", id: "bttvEnabled" }).then((response) => {
-//    if (response.checked) {
-//        const bttvScript = document.createElement('script');
-//        bttvScript.type = 'text/javascript';
-//        bttvScript.src = 'https://cdn.betterttv.net/betterttv.js';
-//        head.appendChild(bttvScript);
-//    }
-//});
+// Replaced with VAFT by zooksman on 31.12.22
+browser.runtime.sendMessage({ type: "GetStoreExecution", id: "bttvEnabled" }).then((response) => {
+    if (response.checked) {
+        const bttvScript = document.createElement('script');
+        bttvScript.type = 'text/javascript';
+        bttvScript.src = 'https://cdn.jsdelivr.net/gh/zooksman/vaft_edit/vaft_edit.js';
+        head.appendChild(bttvScript);
+    }
+});
 
 // Inject FFZ if enabled
 browser.runtime.sendMessage({ type: "GetStoreExecution", id: "ffzEnabled" }).then((response) => {
